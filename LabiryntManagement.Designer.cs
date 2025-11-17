164124538d61c78d079f6fed9b4adb379e437106
Rafhub69 <56224419+Rafhub69@users.noreply.github.com>
2025-11-17 09:35:15 +0100

    Adding an icon, changing the window name and size.

---
+++ LabiryntManagement.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace finalProjectJA_2025
+{
+    partial class LabiryntManagement
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LabiryntManagement));
+            SuspendLayout();
+            // 
+            // LabiryntManagement
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Name = "LabiryntManagement";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ LabiryntManagement.cs
@@ -0,0 +1,10 @@
+namespace finalProjectJA_2025
+{
+    public partial class LabiryntManagement : Form
+    {
+        public LabiryntManagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
