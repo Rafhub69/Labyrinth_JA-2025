2db1732f2a7558dad687e9e77cce3e4e0dfbbf6c
Rafhub69 <56224419+Rafhub69@users.noreply.github.com>
2025-11-18 10:34:28 +0100

    Added two combo boxes and and filled all of them with data.

--- LabiryntManagement.Designer.cs
+++ LabiryntManagement.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace finalProjectJA_2025
+﻿using System.Resources;
+
+namespace finalProjectJA_2025
 {
     partial class LabiryntManagement
     {
@@ -31,8 +33,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LabiryntManagement));
             labelWidth = new Label();
             labelHeight = new Label();
+            labelCellSize = new Label();
+            labelCoresNumber = new Label();
             comboBoxWidth = new ComboBox();
             comboBoxHeight = new ComboBox();
+            comboBoxCellSize = new ComboBox();
+            comboBoxCoresNumber = new ComboBox();
             pictureBoxCentral = new PictureBox();
             pictureBoxBackground = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBoxCentral).BeginInit();
@@ -41,41 +47,81 @@
             // 
             // labelWidth
             // 
-            labelWidth.AutoSize = true;
             labelWidth.BorderStyle = BorderStyle.FixedSingle;
-            labelWidth.Location = new Point(12, 40);
+            labelWidth.Location = new Point(15, 40);
             labelWidth.Name = "labelWidth";
-            labelWidth.Size = new Size(64, 17);
+            labelWidth.Size = new Size(65, 23);
             labelWidth.TabIndex = 1;
             labelWidth.Text = "Szerokość:";
+            labelWidth.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // labelHeight
             // 
-            labelHeight.AutoSize = true;
             labelHeight.BorderStyle = BorderStyle.FixedSingle;
-            labelHeight.Location = new Point(12, 5);
+            labelHeight.Location = new Point(15, 5);
             labelHeight.Name = "labelHeight";
-            labelHeight.Size = new Size(65, 17);
+            labelHeight.Size = new Size(65, 23);
             labelHeight.TabIndex = 0;
             labelHeight.Text = "Wysokość:";
+            labelHeight.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelCellSize
+            // 
+            labelCellSize.BorderStyle = BorderStyle.FixedSingle;
+            labelCellSize.Location = new Point(215, 5);
+            labelCellSize.Name = "labelCellSize";
+            labelCellSize.Size = new Size(120, 23);
+            labelCellSize.TabIndex = 0;
+            labelCellSize.Text = "Wielkość komórek:";
+            labelCellSize.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelCoresNumber
+            // 
+            labelCoresNumber.BorderStyle = BorderStyle.FixedSingle;
+            labelCoresNumber.Location = new Point(215, 40);
+            labelCoresNumber.Name = "labelCoresNumber";
+            labelCoresNumber.Size = new Size(120, 23);
+            labelCoresNumber.TabIndex = 22;
+            labelCoresNumber.Text = "Ilość użytych rdzeni:";
+            labelCoresNumber.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // comboBoxWidth
             // 
             comboBoxWidth.FormattingEnabled = true;
-            comboBoxWidth.Location = new Point(80, 40);
+            comboBoxWidth.Location = new Point(85, 40);
             comboBoxWidth.Name = "comboBoxWidth";
-            comboBoxWidth.Size = new Size(121, 23);
+            comboBoxWidth.Size = new Size(120, 23);
             comboBoxWidth.TabIndex = 3;
+            comboBoxWidth.SelectedIndexChanged += comboBoxWidth_SelectedIndexChanged;
             // 
             // comboBoxHeight
             // 
             comboBoxHeight.DisplayMember = "5";
             comboBoxHeight.FormattingEnabled = true;
-            comboBoxHeight.Location = new Point(81, 5);
+            comboBoxHeight.Location = new Point(85, 5);
             comboBoxHeight.Name = "comboBoxHeight";
-            comboBoxHeight.Size = new Size(121, 23);
+            comboBoxHeight.Size = new Size(120, 23);
             comboBoxHeight.TabIndex = 2;
             comboBoxHeight.ValueMember = "5";
+            comboBoxHeight.SelectedIndexChanged += comboBoxHeight_SelectedIndexChanged;
+            // 
+            // comboBoxCellSize
+            // 
+            comboBoxCellSize.FormattingEnabled = true;
+            comboBoxCellSize.Location = new Point(340, 5);
+            comboBoxCellSize.Name = "comboBoxCellSize";
+            comboBoxCellSize.Size = new Size(121, 23);
+            comboBoxCellSize.TabIndex = 23;
+            comboBoxCellSize.SelectedIndexChanged += comboBoxCellSize_SelectedIndexChanged;
+            // 
+            // comboBoxCoresNumber
+            // 
+            comboBoxCoresNumber.FormattingEnabled = true;
+            comboBoxCoresNumber.Location = new Point(340, 40);
+            comboBoxCoresNumber.Name = "comboBoxCoresNumber";
+            comboBoxCoresNumber.Size = new Size(121, 23);
+            comboBoxCoresNumber.TabIndex = 24;
+            comboBoxCoresNumber.SelectedIndexChanged += comboBoxCoresNumber_SelectedIndexChanged;
             // 
             // pictureBoxCentral
             // 
@@ -103,6 +149,10 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1904, 1041);
+            Controls.Add(comboBoxCoresNumber);
+            Controls.Add(comboBoxCellSize);
+            Controls.Add(labelCoresNumber);
+            Controls.Add(labelCellSize);
             Controls.Add(comboBoxWidth);
             Controls.Add(comboBoxHeight);
             Controls.Add(labelWidth);
@@ -116,18 +166,23 @@
             ((System.ComponentModel.ISupportInitialize)pictureBoxCentral).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBoxBackground).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Label labelWidth;
         private Label labelHeight;
+        private Label labelCellSize;
+        private Label labelCoresNumber;
 
         private ComboBox comboBoxWidth;
         private ComboBox comboBoxHeight;
+        private ComboBox comboBoxCellSize;
+        private ComboBox comboBoxCoresNumber;
 
         private PictureBox pictureBoxCentral;
         private PictureBox pictureBoxBackground;
+
+        
     }
 }
--- LabiryntManagement.cs
+++ LabiryntManagement.cs
@@ -2,9 +2,65 @@ namespace finalProjectJA_2025
 {
     public partial class LabiryntManagement : Form
     {
+        private int[] intCellSize = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+        private int[] intTableSize = new int[255];
+
+        private int maxCoreNumber;
+
         public LabiryntManagement()
         {
             InitializeComponent();
+
+            for (int i = 0; i < intCellSize.Length; i++)
+            {
+                comboBoxCellSize.Items.Add(intCellSize.GetValue(i));
+            }
+
+            maxCoreNumber = 1;
+
+            for (int i = 1; i < 65; i++)
+            {
+                comboBoxCoresNumber.Items.Add(i);
+            }
+
+            for (int i = 0; i < intTableSize.Length; i++)
+            {
+                intTableSize[i] = i + 1;
+            }
+
+            for (int i = 0; i < intTableSize.Length; i++)
+            {
+                comboBoxWidth.Items.Add(intTableSize.GetValue(i).ToString());
+                comboBoxHeight.Items.Add(intTableSize.GetValue(i).ToString());
+            }
+
+            comboBoxHeight.Text = intTableSize[4].ToString();
+            comboBoxWidth.Text = intTableSize[4].ToString();
+
+            comboBoxCellSize.Text = intCellSize[4].ToString();
+            comboBoxCoresNumber.Text = "1";
+
+        }
+
+        private void comboBoxHeight_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxWidth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxCellSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxCoresNumber_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
